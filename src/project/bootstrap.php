c541f1b3c8b1bb215ec42d03163da090b3be8884
Programster <sdpagent@gmail.com>
2018-03-20 12:49:11 +0000

    commiting a fix for potential issues caused by daylight savings time when syncing timestamps.

--- src/project/bootstrap.php
+++ src/project/bootstrap.php
@@ -6,5 +6,9 @@
 require_once(__DIR__ . '/../settings/settings.php');
 require_once(__DIR__ . '/vendor/autoload.php');
 
+// Ensure we are running on UTC time to prevent possible issues with syncing timestamp.
+// https://dba.stackexchange.com/questions/201767/mysql-5-6-error-1292-22007-incorrect-datetime-value-2015-03-29-010112
+date_default_timezone_set('UTC'); 
+
 $classDirs = array(__DIR__);
 $autoloader = new iRAP\Autoloader\Autoloader($classDirs);
